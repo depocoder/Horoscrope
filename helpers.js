3b68113492fb4b105e3273f7587816cf6b4172f3
herypank <60302361+herypank@users.noreply.github.com>
2020-03-19 13:51:55 +0300

    add js and some func, and eggs

---
+++ helpers.js
@@ -0,0 +1,12 @@
+function log_header_click(){
+        console.log("Йоу-Йоу")
+        $('#second-paragraph').html('Секретное_ПОСЛАНИЕ!: МАКБУК ЭТО МОЯ ЖИЗНЬ!!!!!!!!!!!!!')
+    }
+function alert_message_and_secret(){
+    alert("Йоу-Йоу")
+
+    p = $('#second-paragraph');
+    
+    p.html('Секретное_ПОСЛАНИЕ! Спасибо что зашли на сайт!')
+    p.css('color','red')
+}
